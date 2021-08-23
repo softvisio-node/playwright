ab2e95a8e788f19ef94cbffbc284e61b16f9701a
zdm <zdm@softvisio.net>
2021-08-23 09:12:03 +0300

    server path patch added

--- bin/main.js
+++ bin/main.js
@@ -1,10 +1,6 @@
 #!/usr/bin/env node
 
-import module from "module";
-
 const CHROME_PORT = 80;
-
-await patch();
 
 const { "default": playwright } = await import( "#index" );
 
@@ -16,20 +12,4 @@ await playwright.chromium.launchServer( {
     "handleSIGTERM": true,
 } );
 
-console.log( "Chromium: ws://0.0.0.0:" + CHROME_PORT + "/chrome" );
-
-async function patch () {
-    const pwPath = module.createRequire( import.meta.url ).resolve( "playwright-core" ),
-        wsPath = module.createRequire( pwPath ).resolve( "ws" );
-
-    const { "default": ws } = await import( wsPath );
-
-    ws.Server = class extends ws.Server {
-        constructor ( options = {}, callback ) {
-            options.host = "0.0.0.0";
-            options.path = "/";
-
-            super( options, callback );
-        }
-    };
-}
+console.log( "Chromium: ws://0.0.0.0:" + CHROME_PORT + "/" );
