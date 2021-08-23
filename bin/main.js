3825b6c20d241be54247dea4a3a5f780c2d1d4dc
zdm <zdm@softvisio.net>
2021-08-23 15:47:37 +0300

    ws path added

--- bin/main.js
+++ bin/main.js
@@ -1,15 +1,17 @@
 #!/usr/bin/env node
 
 const CHROME_PORT = 80;
+const CHROME_PATH = "/";
 
 const { "default": playwright } = await import( "#index" );
 
 await playwright.chromium.launchServer( {
     "headless": true,
     "port": CHROME_PORT,
+    "wsPath": CHROME_PATH,
     "handleSIGHUP": true,
     "handleSIGINT": true,
     "handleSIGTERM": true,
 } );
 
-console.log( "Chromium: ws://0.0.0.0:" + CHROME_PORT + "/" );
+console.log( "Chromium: ws://0.0.0.0:" + CHROME_PORT + CHROME_PATH );
