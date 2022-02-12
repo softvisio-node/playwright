684f99ee46c336bc1ace725bea04c1612ffb885a
zdm <zdm@softvisio.net>
2022-02-12 21:32:07 +0200

    fix: config api changed

--- bin/install.js
+++ bin/install.js
@@ -1,16 +1,16 @@
 #!/usr/bin/env node
 
 import module from "module";
-import * as config from "#core/config";
+import { readConfig, writeConfig } from "#core/config";
 import path from "path";
 
 const pwPath = module.createRequire( import.meta.url ).resolve( "playwright-core" ),
     pkgPath = path.join( path.dirname( pwPath ), "package.json" ),
-    pkg = config.read( pkgPath );
+    pkg = readConfig( pkgPath );
 
 pkg.exports["./*"] = "./*";
 delete pkg.exports["./"];
 
-config.write( pkgPath, pkg, { "readable": true } );
+writeConfig( pkgPath, pkg, { "readable": true } );
 
 console.log( "Playwright exports patched:", pkg.exports );
