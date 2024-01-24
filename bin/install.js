fa6362159d7f7510cc50a99d0e20c77951d8c625
zdm <zdm@softvisio.net>
2024-01-24 12:51:23 +0200

    fix: linted

--- bin/install.js
+++ bin/install.js
@@ -8,8 +8,8 @@ const pwPath = module.createRequire( import.meta.url ).resolve( "playwright-core
     pkgPath = path.join( path.dirname( pwPath ), "package.json" ),
     pkg = readConfig( pkgPath );
 
-pkg.exports["./*"] = "./*";
-delete pkg.exports["./"];
+pkg.exports[ "./*" ] = "./*";
+delete pkg.exports[ "./" ];
 
 writeConfig( pkgPath, pkg, { "readable": true } );
 
